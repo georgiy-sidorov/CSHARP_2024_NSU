4bc9938ef8680a1e0a3708c731541b3ee74e48bb
george <g.sidorov@trueengineering.ru>
2024-09-02 15:58:20 +0700

    initial

---
+++ Nsu.HackathonProblem.Contracts/Nsu.HackathonProblem.Contracts/ITeamBuildingStrategy.cs
@@ -0,0 +1,13 @@
+﻿namespace Nsu.HackathonProblem.Contracts
+{
+    public interface ITeamBuildingStrategy
+    {
+        /// <summary>
+        /// Распределяет тиилидов и джунов по командам
+        /// </summary>
+        /// <param name="teamLeads">Тимлиды</param>
+        /// <param name="juniors">Джуны</param>
+        /// <returns>Список команд</returns>
+        IEnumerable<Team> BuildTeams(IEnumerable<Employee> teamLeads, IEnumerable<Employee> juniors);
+    }
+}
